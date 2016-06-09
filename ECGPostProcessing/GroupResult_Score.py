d047d7465f9c1a0724d9d1e2ca5d57cec8e31137
AlexG31 <AresEyE@163.com>
2016-06-09 15:17:51 +0800

    evaluation with prediction score

---
+++ ECGPostProcessing/GroupResult_Score.py
@@ -0,0 +1,197 @@
+#encoding:utf-8
+"""
+ECG Medical Evaluation Module
+Author : Gaopengfei
+"""
+import os
+import sys
+import json
+import glob
+import math
+import pickle
+import random
+import time
+import pdb
+
+import numpy as np
+## machine learning methods
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+# project homepath
+# 
+curfilepath =  os.path.realpath(__file__)
+curfolderpath = os.path.dirname(curfilepath)
+projhomepath = os.path.dirname(curfolderpath)
+print 'projhomepath:',projhomepath
+# configure file
+# conf is a dict containing keys
+with open(os.path.join(projhomepath,'ECGconf.json'),'r') as fin:
+    conf = json.load(fin)
+sys.path.append(projhomepath)
+#
+# my project components
+import RFclassifier.extractfeature.extractfeature as extfeature
+import QTdata.loadQTdata as QTdb
+from RFclassifier.evaluation import ECGstatistics
+import RFclassifier.ECGRF as ECGRF 
+
+class ResultFilter:
+    def __init__(self,recres):
+        self.recres = recres
+        pass
+    def group_local_result(self,recres = None,white_del_thres = 20,cp_del_thres = 0):
+        #
+        # 参数说明：1.white_del_thres是删除较小白色组的阈值
+        #           2.cp_del_thres是删除较小其他关键点组的阈值
+        # Multiple prediction point -> single point output
+        ## filter output for evaluation results
+        #
+        # parameters
+        # recres:
+        # [(pos,label,score),...]
+        #
+        # the number of the group must be greater than:
+        #
+        # default parameter
+        if recres is None:
+            recres = self.recres
+
+        # filtered test result
+        frecres = []
+        # in var
+        prev_label = None
+        posGroup = []
+        #----------------------
+        # [pos,label] in recres
+        #----------------------
+        for pos,label,score in recres:
+            if prev_label is not None:
+                if label != prev_label:
+                    frecres.append((prev_label,posGroup))
+                    posGroup = []
+                
+            prev_label = label
+            posGroup.append((pos,score))
+        # add last label group
+        if len(posGroup)>0:
+            frecres.append((prev_label,posGroup))
+        #======================
+        # 1.删除比较小的白色组和其他组(different threshold)
+        # 2.合并删除后的相邻同色组
+        #======================
+        filtered_local_res = []
+        for label,posGroup in frecres:
+            if label == 'white' and len(posGroup) <= white_del_thres:
+                continue
+            if label != 'white' and len(posGroup) <= cp_del_thres:
+                continue
+            # can merge backward?
+            if len(filtered_local_res)>0 and filtered_local_res[-1][0] == label:
+                filtered_local_res[-1][1].extend(posGroup)
+            else:
+                filtered_local_res.append((label,posGroup))
+
+        frecres = filtered_local_res
+        # weighted sum with score
+        weighted_reslist = []
+        for label,posGroup in frecres:
+            poslist,scorelist = zip(*posGroup)
+            if len(poslist) == 1:
+                weighted_reslist.append((poslist[0],label))
+                continue
+            scoresum = 0.0
+            pos_weighted_sum = 0.0
+            for pos,score in posGroup:
+                scoresum += score
+                pos_weighted_sum += float(pos)*score
+            mean_pos = int(pos_weighted_sum/scoresum)
+            weighted_reslist.append((mean_pos,label))
+
+        return weighted_reslist
+    def syntax_filter(self,reslist = None,fs = 250,Max_Len_Ratio = 3.0/25):
+        # nest function
+        def next_label(label):
+            ## syntax next label
+            ret = None
+            if label == 'T':
+                ret = 'Toffset'
+            elif label == 'R':
+                ret = 'Roffset'
+            elif label == 'P':
+                ret = 'Poffset'
+            elif label == 'Tonset':
+                ret = 'T'
+            elif label == 'Toffset':
+                ret = 'Ponset'
+            elif label == 'Ronset':
+                ret = 'R'
+            elif label == 'Roffset':
+                ret = 'Tonset'
+            elif label == 'Ponset':
+                ret = 'P'
+            elif label == 'Poffset':
+                ret = 'Ronset'
+            else:# white
+                ret = None
+            return ret
+        # parameter
+        if reslist is None:
+            reslist = self.recres
+        if len(reslist) == 0:
+            return []
+        # const
+        Max_Len = Max_Len_Ratio*fs
+        Offset_Len = fs/25
+        # filtered result
+        flt_res = []
+        flt_res.append(reslist[0])
+
+        # prev label and pos
+        prev_label = reslist[0][1]
+        prev_pos = reslist[0][0]
+        prev_ind = 0
+
+        # ----------------------
+        # filtering
+        # ----------------------
+        N_reslist = len(reslist)
+        while prev_ind < N_reslist:
+            # find next non white point
+            while prev_label == 'white' and prev_ind+1<N_reslist:
+                prev_ind += 1
+                prev_pos,prev_label = reslist[prev_ind]
+                flt_res[-1] = reslist[prev_ind]
+            if prev_ind+1 >= N_reslist:
+                # no next
+                break
+            # matched next ind
+            match_ind = -1
+            cur_Max_Dist = Max_Len
+            if 'offset' in prev_label:
+                cur_Max_Dist = Offset_Len
+            for nxt_ind in xrange(prev_ind,N_reslist):
+                curpos,curlabel = reslist[nxt_ind]
+                if curpos - prev_pos > cur_Max_Dist:
+                    # not a label match
+                    if match_ind == -1:
+                        pass
+                        #print 'prev_ind = {},nxt_ind = {}'.format(prev_label,nxt_ind)
+                        #print 'reslist[{}:{}] = {}'.format(prev_ind,nxt_ind+1,reslist[prev_ind:nxt_ind+1])
+                        #pdb.set_trace()
+                    break
+                if next_label(prev_label) == curlabel:
+                    match_ind = nxt_ind
+            if match_ind == -1:
+                if nxt_ind >= N_reslist:
+                    break
+                else:
+                    # False negtive
+                    match_ind = nxt_ind
+            prev_ind = match_ind
+            flt_res.append(reslist[match_ind])
+            prev_pos,prev_label = reslist[match_ind]
+
+        return flt_res
+
+
---
+++ EvaluationSchemes/PaperResultEvaluation/sel30_ECGPloter.py
@@ -0,0 +1,194 @@
+#encoding:utf-8
+import os
+import sys
+import pickle
+import json
+import glob
+import marshal
+import pdb
+import bisect
+import matplotlib.pyplot as plt
+
+# project homepath
+curfilepath =  os.path.realpath(__file__)
+curfolderpath = os.path.dirname(curfilepath)
+projhomepath = os.path.dirname(curfolderpath)
+projhomepath = os.path.dirname(projhomepath)
+# configure file
+with open(os.path.join(projhomepath,'ECGconf.json'),'r') as fin:
+    conf = json.load(fin)
+sys.path.append(projhomepath)
+
+#import QTdata.loadQTdata.QTloader as QTloader
+from QTdata.loadQTdata import QTloader 
+from RFclassifier.ECGRF import ECGrf as ECGRF
+from RFclassifier.evaluation import ECGstatistics as ECGstats
+
+class ECGResultPloter:
+    def __init__(self,rawsig,testresult = None,recordname = None):
+        self.rawsig = rawsig
+        self.testresult = testresult
+        self.QTdb = QTloader()
+        self.recordname = recordname
+        self.plotMarkerlist = [
+                'ro',
+                'go',
+                'bo',
+                'r<',
+                'r>',
+                'g<',
+                'g>',
+                'b<',
+                'b>',
+                'w.']
+        pass
+    def PlotMarker2Label(self,label):
+        if label == 'ro':
+            mker = 'T wave peak'
+        elif label == 'go':
+            mker = 'R wave peak'
+        elif label == 'bo':
+            mker = 'P wave peak'
+        elif label == 'r<':
+            mker = 'T onset'
+        elif label == 'r>':
+            mker = 'T offset'
+        elif label == 'g<':
+            mker = 'Ronset'
+        elif label == 'g>':
+            mker = 'Roffset'
+        elif label == 'b<':
+            mker = 'Ponset'
+        elif label == 'b>':
+            mker = 'Poffset'
+        else:# white
+            mker = 'non-characteristic points'
+        return mker
+    def Label2PlotMarker(self,label):
+        if label == 'T':
+            mker = 'ro'
+        elif label == 'R':
+            mker = 'go'
+        elif label == 'P':
+            mker = 'bo'
+        elif label == 'Tonset':
+            mker = 'r<'
+        elif label == 'Toffset':
+            mker = 'r>'
+        elif label == 'Ronset':
+            mker = 'g<'
+        elif label == 'Roffset':
+            mker = 'g>'
+        elif label == 'Ponset':
+            mker = 'b<'
+        elif label == 'Poffset':
+            mker = 'b>'
+        else:# white
+            mker = 'w.'
+        return mker
+        
+    def plot(self,plotTitle = None,dispRange = None,plotFig = 1,plotShow = True,AdditionalPlot = None):
+        # AdditionalPlot
+        # [ 
+        #   [plotmarker,legend,x,(y)],
+        #   ...
+        # ]
+        # display range
+        if dispRange is not None:
+            dispsig = self.rawsig[dispRange[0]:dispRange[1]]
+        else:
+            dispsig = self.rawsig
+        # clear graph
+        plt.clf()
+        plt.figure(plotFig)
+        plt.plot(dispsig)
+        if self.testresult is not None and len(self.testresult)>0:
+            if dispRange is not None:
+                dispres = self.testresult
+                (poslist,reslabellist) = zip(*dispres)
+                resrange_L = bisect.bisect_left(poslist,dispRange[0])
+                resrange_R = bisect.bisect_left(poslist,dispRange[1])
+                poslist = poslist[resrange_L:resrange_R]
+                reslabellist = reslabellist[resrange_L:resrange_R]
+            else:
+                dispres = self.testresult
+                # convert labels to plot markers
+                (poslist,reslabellist) = zip(*dispres)
+            resplotmarkerlist = map(self.Label2PlotMarker,reslabellist)
+            for mker in self.plotMarkerlist:
+                mkerposlist = []
+                Amplist = []
+                for mi,resmker in enumerate(resplotmarkerlist):
+                    if resmker == mker:
+                        if dispRange is None:
+                            mkerpos = poslist[mi]
+                        else:
+                            mkerpos = poslist[mi] - dispRange[0]
+                        mkerposlist.append(mkerpos)
+                        # poslist is signal global index
+                        Amplist.append(dispsig[mkerpos])
+                if len(mkerposlist)>0:
+                    # have avaliable mker to plot
+                    if mker == 'w.':
+                        plt.plot(mkerposlist,Amplist,mker,markerfacecolor = 'b',markersize=10,label = '{}'.format(self.PlotMarker2Label(mker)))
+                    else:
+                        plt.plot(mkerposlist,Amplist,mker,markersize=10,label = '{}'.format(self.PlotMarker2Label(mker)))
+        #=====================
+        # plot AdditionalPlot
+        # [(plot_marker,label,x,[y])]
+        #=====================
+        if AdditionalPlot is not None:
+            for plotElem in AdditionalPlot:
+                plotElem_x = plotElem[2]
+                if len(plotElem) == 3:
+                    plotElem_y = map(lambda x:dispsig[int(x)],plotElem_x)            
+                elif len(plotElem) == 4:
+                    plotElem_y = plotElem[3]
+                else:
+                    raise Exception('len(plotElem) should be 3 or 4!')
+                # plot 
+                plt.plot(plotElem_x,plotElem_y,plotElem[0],markersize = 10,label = plotElem[1])
+        if plotTitle is not None:
+            plt.title(plotTitle)
+        # show legend
+        plt.legend(numpoints = 1)
+        plt.xlabel('Samples')
+        plt.ylabel('Amplitude')
+        if plotShow is True:
+            plt.show()
+    def plotAndsave(self,savefilefullpath,plotTitle = None,dispRange = None):
+        # display range
+        if dispRange is not None:
+            dispsig = self.rawsig[dispRange[0]:dispRange[1]]
+        else:
+            dispsig = self.rawsig
+        plt.figure(num = 1,figsize = (20,10))
+        plt.plot(dispsig)
+        if self.testresult is not None:
+            if dispRange is not None:
+                dispres = self.testresult[dispRange[0]:dispRange[1]]
+            else:
+                dispres = self.testresult
+            # convert labels to plot markers
+            (poslist,reslabellist) = zip(*dispres)
+            resplotmarkerlist = map(self.Label2PlotMarker,reslabellist)
+            for mker in self.plotMarkerlist:
+                mkerposlist = []
+                Amplist = []
+                for mi,resmker in enumerate(resplotmarkerlist):
+                    if resmker == mker:
+                        if dispRange is None:
+                            mkerpos = poslist[mi]
+                        else:
+                            mkerpos = poslist[mi] - dispRange[0]
+                        mkerposlist.append(mkerpos)
+                        # poslist is signal global index
+                        Amplist.append(dispsig[mkerpos])
+                if len(mkerposlist)>0:
+                    # have avaliable mker to plot
+                    plt.plot(mkerposlist,Amplist,mker)
+            if plotTitle is not None:
+                plt.title(plotTitle)
+            plt.savefig(savefilefullpath+'.png')
+        # clear graph
+        plt.clf()
