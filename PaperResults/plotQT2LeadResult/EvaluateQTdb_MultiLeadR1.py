1401f7939c96b8f3bee2e8d12f5cf2c157de80d5
AlexG31 <AresEyE@163.com>
2016-06-30 17:52:34 +0800

    2Lead result Postprocessing and evaluation

---
+++ PaperResults/plotQT2LeadResult/EvaluateQTdb_MultiLeadR1.py
@@ -0,0 +1,460 @@
+#encoding:utf-8
+"""
+ECG Evaluation Module
+Author : Gaopengfei
+"""
+import os
+import sys
+import json
+import glob
+import math
+import pickle
+import random
+import time
+import pdb
+
+import numpy as np
+## machine learning methods
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+# project homepath
+# 
+curfilepath =  os.path.realpath(__file__)
+curfolderpath = os.path.dirname(curfilepath)
+projhomepath = os.path.dirname(curfolderpath)
+projhomepath = os.path.dirname(projhomepath)
+# configure file
+# conf is a dict containing keys
+with open(os.path.join(projhomepath,'ECGconf.json'),'r') as fin:
+    conf = json.load(fin)
+sys.path.append(projhomepath)
+#
+# my project components
+import RFclassifier.extractfeature.extractfeature as extfeature
+from QTdata.loadQTdata import QTloader
+from RFclassifier.evaluation import ECGstatistics
+import RFclassifier.ECGRF as ECGRF 
+from ECGPostProcessing.GroupResult import ResultFilter
+
+
+def TestingAndSaveResult():
+    sel1213 = conf['sel1213']
+    time0 = time.time()
+    rf = ECGRF.ECGrf()
+    rf.training(sel1213[0:1])
+    time1 = time.time()
+    print 'Training time:',time1-time0
+    ## test
+    rf.testmdl(reclist = sel1213[0:1])
+
+def debug_show_eval_result(\
+            picklefilename,\
+            target_recname = None,\
+            singleRecordFile = False\
+        ):
+    with open(picklefilename,'r') as fin:
+        Results = pickle.load(fin)
+    # convert to a list
+    if singleRecordFile == True:
+        Results = [Results,]
+    for recind in xrange(0,len(Results)):
+        # only plot target rec
+        if target_recname is not None:
+            print 'Current FileName: {}'.format(Results[recind][0])
+            if Results[recind][0]!= target_recname:
+                return 
+        fResults = ECGRF.ECGrf.resfilter(Results)
+        #
+        # show filtered results & raw results
+        # Evaluate prediction result statistics
+        #
+
+        ECGstats = ECGstatistics(fResults[recind:recind+1])
+        ECGstats.eval(debug = False)
+        ECGstats.dispstat0()
+        ECGstats.plotevalresofrec(Results[recind][0],Results)
+
+def LOOT_Eval(RFfolder):
+    reslist = glob.glob(os.path.join(\
+            RFfolder,'*.out'))
+    FN =  {
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    Err = {
+                'err':[],
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+
+    for fi,fname in enumerate(reslist):
+        with open(fname,'r') as fin:
+            Results = pickle.load(fin)
+        fResults = ECGRF.ECGrf.resfilter(Results)
+        # show filtered results & raw results
+        #for recname , recRes in Results:
+
+        # Evaluate prediction result statistics
+        #
+        ECGstats = ECGstatistics(fResults[0:1])
+        pErr,pFN = ECGstats.eval(debug = False)
+        for kk in Err:
+            Err[kk].extend(pErr[kk])
+        for kk in FN:
+            FN[kk].extend(pFN[kk])
+
+    # write to log file
+    EvalLogfilename = os.path.join(curfolderpath,'res.log')
+    ECGstatistics.dispstat0(\
+            pFN = FN,\
+            pErr = Err,\
+            LogFileName = EvalLogfilename,\
+            LogText = 'Statistics of Results in [{}]'.\
+                format(RFfolder)\
+            )
+    ECGstats.stat_record_analysis(pErr = Err,pFN = FN,LogFileName = EvalLogfilename)
+
+def TestN_Eval(RFfolder,output_log_filename = os.path.join(curfolderpath,'res.log')):
+
+    # test result file list
+    picklereslist = glob.glob(os.path.join(RFfolder,'*.out'))
+    # struct Init
+    FN =  {
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    Err = {
+                'err':[],
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    #========================================
+    # select best round to compare with refs
+    #========================================
+    bRselector = BestRoundSelector()
+
+    for fi,fname in enumerate(picklereslist):
+        
+        with open(fname,'rU') as fin:
+            Results = pickle.load(fin)
+        # filter result
+        fResults = ECGRF.ECGrf.resfilter(Results)
+        # show filtered results & raw results
+        #for recname , recRes in Results:
+
+        # Evaluate prediction result statistics
+        #
+        ECGstats = ECGstatistics(fResults)
+        pErr,pFN = ECGstats.eval(debug = False)
+        # one test Error stat
+        print '[picle filename]:{}'.format(fname)
+        print '[{}] files left.'.format(len(picklereslist) - fi)
+        evallabellist,evalstats = ECGstatistics.dispstat0(\
+                pFN = pFN,\
+                pErr = pErr\
+                )
+        # select best Round 
+        numofFN = len(pFN['pos'])
+        if numofFN == 0:
+            ExtraInfo = 'Best Round ResultFileName[{}]\nTestSet :{}\n#False Negtive:{}\n'.format(fname,[x[0] for x in Results],numofFN)
+            bRselector.input(evallabellist,evalstats,ExtraInfo = ExtraInfo)
+
+        for kk in Err:
+            Err[kk].extend(pErr[kk])
+        for kk in FN:
+            FN[kk].extend(pFN[kk])
+
+    # write to log file
+    #EvalLogfilename = os.path.join(curfolderpath,'res.log')
+    EvalLogfilename = output_log_filename
+    # display error stat for each label & save results to logfile
+    ECGstatistics.dispstat0(\
+            pFN = FN,\
+            pErr = Err,\
+            LogFileName = EvalLogfilename,\
+            LogText = 'Statistics of Results in FilePath [{}]'.format(RFfolder)\
+            )
+    with open(os.path.join(projhomepath,'tmp','Err.txt'),'w') as fout:
+        pickle.dump(Err,fout)
+    # find best round
+    bRselector.dispBestRound()
+    bRselector.dumpBestRound(EvalLogfilename)
+
+    ECGstats.stat_record_analysis(pErr = Err,pFN = FN,LogFileName = EvalLogfilename)
+
+class BestRoundSelector:
+    '''Select the best Round'''
+    std_max = {
+            'P': 20,
+            'R': 20,
+            'T': 20,
+            'Ponset': 14.3,
+            'Poffset': 13.9,
+            'Ronset': 8.8,
+            'Roffset': 9.2,
+            'Toffset': 18.6
+            }
+    mean_max = {
+            'P': 4,
+            'R': 6,
+            'T': 5,
+            'Ponset': 2.5,
+            'Poffset': 3.1,
+            'Ronset': 2.4,
+            'Roffset': 4.7,
+            'Toffset': 2.8
+            }
+    # select the best Round Unit (ms)
+    def __init__(self):
+        self.labellist = []
+        self.beststats = []
+        self.sumstd = None
+        self.ExtraInfo = ""
+        self.ExcMean = -1
+        self.ExcStd = -1
+        self.debugcnt = 0
+        
+    def input(self,labellist,stats,ExtraInfo = ''):
+        # if better than current stats,then keep them
+        if False == self.all_lower_than_standard(labellist,stats):
+            return
+        excmean,excstd = self.getnumof_Exceedingstats(labellist,stats)
+        meanlist,stdlist = zip(*stats)
+        # sum of std
+        sumstd = sum(stdlist)
+        if self.labellist == 0 or self.sumstd is None or (self.ExcMean>excmean and self.ExcStd >= excstd) or (self.ExcMean>=excmean and self.ExcStd > excstd) or ((self.ExcMean>=excmean and self.ExcStd >= excstd) and self.sumstd > sumstd):
+            self.labellist = labellist
+            self.beststats = stats
+            self.sumstd = sumstd
+            self.ExtraInfo = ExtraInfo
+            self.ExcMean,self.ExcStd = excmean,excstd
+
+        
+    def all_lower_than_standard(self,labellist,stats):
+        # class global threshold
+        std_max = self.std_max
+        mean_max = self.mean_max
+
+        meanlist,stdlist = zip(*stats)
+        label_std_list = zip(labellist,stdlist)
+        label_mean_list = zip(labellist,meanlist)
+        # cannot have inf/nan
+        for meanval in meanlist:
+            if np.isnan(meanval) or np.isinf(meanval):
+                print 'this record has nan/inf,not included in best round selection.'
+                return False
+        # m and std Upper bound
+        for label,std in label_std_list:
+            if std > std_max[label]+4:
+                return False
+        for label,mean in label_mean_list:
+            if abs(mean) > mean_max[label]+2:
+                return False
+        # the less number of bad m the better
+        badn_m,badn_std = self.getnumof_Exceedingstats(labellist,stats)
+
+        self.debugcnt += 1
+        return True
+    def getnumof_Exceedingstats(self,labellist,stats):
+        # class global threshold
+        std_max = self.std_max
+        mean_max = self.mean_max
+
+        meanlist,stdlist = zip(*stats)
+        label_std_list = zip(labellist,stdlist)
+        label_mean_list = zip(labellist,meanlist)
+        # the less number of bad m the better
+        badn_m,badn_std = 0,0
+        for label,std in label_std_list:
+            if std > std_max[label]:
+                badn_std += 1
+        for label,mean in label_mean_list:
+            if abs(mean) > mean_max[label]:
+                badn_m += 1
+        return (badn_m,badn_std)
+       
+    def dumpBestRound(self,LogFileName):
+        res = zip(self.labellist,self.beststats)
+        with open(LogFileName,'a') as fout:
+            fout.write('\n{}\n'.format('='*40))
+            fout.write('{}\n'.format(self.ExtraInfo))
+            fout.write('\n>>Best Round statistics Format:[label: (mean,std) ]<<\n')
+            fout.write('\n number of error exceeds refs: [mean:{},std:{}]\n'.format(self.ExcMean,self.ExcStd))
+            for elem in res:
+                fout.write('{}\n'.format(elem))
+            
+    def dispBestRound(self):
+        res = zip(self.labellist,self.beststats)
+        print '='*40
+        print self.ExtraInfo
+        print '>>Best Round statistics [label: (mean,std) ]<<'
+        print 'Error Exceedings: [mean:{},std:{}]'.format(self.ExcMean,self.ExcStd)
+        print 'debugcnt:',self.debugcnt
+        for elem in res:
+            print elem
+        
+
+def EvalQTdbResults(resultfilelist,OutputFolder):
+    if resultfilelist==None or len(resultfilelist)==0:
+        print "Empty result file list!"
+        return None
+    FN =  {
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    FP =  {
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    Err = {
+                'err':[],
+                'pos':[],
+                'label':[],
+                'recname':[]
+                }
+    #========================================
+    # select best round to compare with refs
+    #========================================
+    bRselector = BestRoundSelector()
+    #InvalidRecordList = conf['InvalidRecords']
+
+    # for each record test result
+    for fi,fname in enumerate(resultfilelist):
+        
+        print 'json load :',fname
+        with open(fname,'rU') as fin:
+            Results = json.load(fin)
+            Results = Results[0]
+        # skip invalid records
+        currecordname = Results[0]
+        #if currecordname in InvalidRecordList:
+            #continue
+        # ==================================
+        # filter result of QT
+        # ==================================
+        reslist = Results[1]
+        resfilter = ResultFilter(reslist)
+        reslist = resfilter.group_local_result(cp_del_thres = 1)
+        reslist = resfilter.syntax_filter(reslist)
+        fResults = (Results[0],reslist)
+
+        fResults = [fResults,]
+        # show filtered results & raw results
+        #for recname , recRes in Results:
+
+        # Evaluate prediction result statistics
+        #
+        ECGstats = ECGstatistics(fResults)
+        pErr,pFN = ECGstats.eval(debug = False)
+        # get False Positive
+        pFP = ECGstats.pFP
+        # one test Error stat
+        print '[picle filename]:{}'.format(fname)
+        print '[{}] files left.'.format(len(resultfilelist) - fi)
+        evallabellist,evalstats = ECGstatistics.dispstat0(pFN = pFN,pErr = pErr)
+        # select best Round 
+        numofFN = len(pFN['pos'])
+        if numofFN == 0:
+            ExtraInfo = 'Best Round ResultFileName[{}]\nTestSet :{}\n#False Negtive:{}\n'.format(fname,[x[0] for x in Results],numofFN)
+            bRselector.input(evallabellist,evalstats,ExtraInfo = ExtraInfo)
+        # ==============================================
+        for kk in Err:
+            Err[kk].extend(pErr[kk])
+        for kk in FN:
+            FN[kk].extend(pFN[kk])
+        for kk in FP:
+            FP[kk].extend(pFP[kk])
+
+    #====================================
+    # write to log file
+    #EvalLogfilename = os.path.join(curfolderpath,'res.log')
+    output_log_filename = os.path.join(OutputFolder,'RecordResults.log')
+    EvalLogfilename = output_log_filename
+    # display error stat for each label & save results to logfile
+    ECGstatistics.dispstat0(pFN = FN,pErr = Err,LogFileName = EvalLogfilename,LogText = 'Statistics of Results in FilePath [{}]'.format(os.path.split(resultfilelist[0])[0]),OutputFolder = OutputFolder)
+    with open(os.path.join(curfolderpath,'Err.txt'),'w') as fout:
+        pickle.dump(Err,fout)
+    # find best round
+    bRselector.dispBestRound()
+    bRselector.dumpBestRound(EvalLogfilename)
+
+    ECGstats.stat_record_analysis(pErr = Err,pFN = FN,LogFileName = EvalLogfilename)
+    # write csv file
+    outputfilename = os.path.join(OutputFolder,'FalsePositive.csv')
+    ECGstats.FP2CSV(FP,Err,outputfilename)
+    # False Negtive
+    outputfilename = os.path.join(OutputFolder,'FalseNegtive.csv')
+    ECGstats.FN2CSV(FN,Err,outputfilename)
+
+def getresultfilelist(RFfolder):
+    # ================================
+    #  get the result file path list
+    # ================================
+    reslist = glob.glob(os.path.join(\
+           RFfolder,'*'))
+    ret = []
+    non_result_extensions = ['out','json','tmp','log','mdl','txt','rar']
+    for fpath in reslist:
+        cur_extension = fpath.split('.')[-1]
+        # not folder
+        if os.path.isdir(fpath) == True:
+            continue
+        if cur_extension in non_result_extensions:
+            continue
+        result_filename = os.path.split(fpath)[-1]
+        if result_filename.startswith('result_') == False:
+            continue
+        print 'add result file to analysis: {}'.format(fpath) 
+        ret.append(fpath)
+    return ret
+    
+    
+def get_training_testing_record_number(RFfolder):
+    # get the number of training & testing records
+    traininglist = conf['selQTall0']
+    testlist = getresultfilelist(RFfolder)
+    # filter testlist
+    eval_testlist = []
+    invalidlist = conf['InvalidRecords']
+    for testrec in testlist:
+        print 'processing record:{}'.format(testrec)
+        with open(testrec,'r') as fin:
+            (recname,resdata) = pickle.load(fin)
+        if recname not in invalidlist:
+            eval_testlist.append(recname)
+
+    num_training = len(traininglist)
+    num_testing = len(eval_testlist)
+    print 'num of training rec:{}'.format(num_training)
+    print 'num of testing rec:{}'.format(num_testing)
+    print 'total record number : {}'.format(num_training+num_testing)
+
+if __name__ == '__main__':
+        RFfolder = os.path.join(\
+               projhomepath,\
+               'TestResult',\
+               'paper',\
+               'MultiRound1',\
+               'Round*')
+        ResultFolders = glob.glob(RFfolder)
+        #getRRhisto()
+        #sys.exit()
+        #get number of test/training records
+        # =========================
+        # 分析训练和测试的record数目
+        # =========================
+        #get_training_testing_record_number(RFfolder)
+        # 计算误差
+        for res_folder in ResultFolders:
+            foldername = os.path.split(res_folder)[-1]
+            OutputFolder = os.path.join(curfolderpath,'MultiLead1','eval_{}'.format(foldername))
+            os.mkdir(OutputFolder)
+            EvalQTdbResults(getresultfilelist(res_folder),OutputFolder)
+
---
+++ PaperResults/plotQT2LeadResult/Evaluation2Leads.py
@@ -0,0 +1,304 @@
+#encoding:utf-8
+"""
+ECG Evaluation Module
+Author : Gaopengfei
+"""
+import os
+import sys
+import json
+import glob
+import math
+import pickle
+import random
+import bisect
+import time
+import pdb
+
+import numpy as np
+## machine learning methods
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+
+# project homepath
+# 
+curfilepath =  os.path.realpath(__file__)
+curfolderpath = os.path.dirname(curfilepath)
+projhomepath = os.path.dirname(curfolderpath)
+projhomepath = os.path.dirname(projhomepath)
+# configure file
+# conf is a dict containing keys
+with open(os.path.join(projhomepath,'ECGconf.json'),'r') as fin:
+    conf = json.load(fin)
+sys.path.append(projhomepath)
+#
+# my project components
+from QTdata.loadQTdata import QTloader
+#from RFclassifier.evaluation import ECGstatistics
+#from ECGPostProcessing.PeakDetection import ECG_PeakDetection as PeakDetectionFilter
+
+
+possibleLabels = [
+            'P',
+            'R',
+            'T',
+            'Ponset',
+            'Poffset',
+            'Ronset',
+            'Roffset',
+            'Toffset',
+            ]
+
+class Evaluation2Leads:
+    def __init__(self):
+        print '[Warning]Evaluator will convert prediction indexes to Integer.'
+        self.QTdb = QTloader()
+        self.labellist = []
+        self.expertlist = []
+        self.recname = None
+        self.prdMatchList = []
+        self.expMatchList = []
+        # color schemes
+        tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+        self.colors = []
+        for color_tri in tableau20:
+            self.colors.append((color_tri[0]/255.0,color_tri[1]/255.0,color_tri[2]/255.0))
+
+        # error List
+        # [expPos - prdPos,...]
+        self.errList = None
+    def clear(self):
+        # error List
+        # [expPos - prdPos,...]
+        self.errList = None
+        self.labellist = []
+        self.expertlist = []
+        self.recname = None
+        self.prdMatchList = []
+        self.expMatchList = []
+
+    def loadlabellist(self,filename,TargetLabel):
+        '''Load label list with Target Label from json file.'''
+
+        with open(filename,'r') as fin:
+            Res = json.load(fin)
+        #reclist = Res.keys()
+
+        #if len(reclist) ==0:
+            #raise Exception('No result data in file:'+filename+'!')
+        #elif len(reclist)>1:
+            #print 'Rec List:',reclist
+            #print '[Warning] Multiple result data, only the 1st one is used!'
+
+        self.recname = Res['recname']
+        print '>>loading recname:',self.recname
+
+        self.leadResults= Res['LeadResult']
+        self.leadPosList = (self.leadResults[0][TargetLabel],self.leadResults[1][TargetLabel])
+
+        # convert values to int
+        #self.labellist = map(lambda x:int(x),self.labellist)
+
+        expertlist = self.QTdb.getexpertlabeltuple(self.recname)
+        self.expertlist = map(lambda x:x[0],filter(lambda x:x[1]==TargetLabel,expertlist))
+
+    def getMatchList(self,prdposlist,MaxMatchDist = 50):
+        '''Return the match result between prdposlist and expertlist.'''
+        # Max Match Dist
+
+        prdposlist.sort()
+        self.expertlist.sort()
+
+        prdMatchList = [-1,]*len(prdposlist)
+        expMatchList = [-1,]*len(self.expertlist)
+
+        N_prdList = len(prdposlist)
+        for expInd,exppos in enumerate(self.expertlist):
+            insertPos = bisect.bisect_left(prdposlist,exppos)
+            left_ind = insertPos-1
+            right_ind = insertPos
+            matchInd = -1
+            matchDist = -1
+            if left_ind<0:
+                matchInd = right_ind
+                matchDist = abs(exppos-prdposlist[right_ind])
+            elif right_ind>=N_prdList:
+                matchInd = left_ind
+                matchDist = abs(exppos-prdposlist[left_ind])
+            else:
+                leftDist = abs(exppos-prdposlist[left_ind])
+                rightDist = abs(exppos-prdposlist[right_ind])
+                if leftDist>rightDist:
+                    matchInd = right_ind
+                    matchDist = rightDist
+                else:
+                    matchInd = left_ind
+                    matchDist = leftDist
+            if matchInd == -1 or matchDist>=MaxMatchDist:
+                expMatchList[expInd] = -1
+            else:
+                expMatchList[expInd] = matchInd
+                prdMatchList[matchInd] = expInd
+        return (expMatchList,prdMatchList)
+
+    def evaluate(self,TargetLabel):
+        self.prdMatchList = []
+        self.expMatchList = []
+
+        # get 2 Leads match lists
+        expMatchList,prdMatchList = self.getMatchList(self.leadPosList[0])
+        self.prdMatchList.append(prdMatchList)
+        self.expMatchList.append(expMatchList)
+        expMatchList,prdMatchList = self.getMatchList(self.leadPosList[1])
+        self.prdMatchList.append(prdMatchList)
+        self.expMatchList.append(expMatchList)
+
+        # get error statistics
+        self.get_errList()
+
+    def getFNlist(self):
+        '''Return total number of False Negtives.'''
+        return self.FNcnt
+    def getFPlist(self):
+        return -1
+
+    def plot_evaluation_result(self):
+        sigStruct = self.QTdb.load(self.recname)
+        rawSig = sigStruct['sig']
+
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(rawSig)
+
+        # plot expert labels
+        expPosList = self.expertlist
+        plt.plot(expPosList,map(lambda x:rawSig[x],expPosList),'d',color = self.colors[2],markersize = 12,label = 'Expert Label')
+        # unmatched expert labels
+        #FNPosList = map(lambda x:x[0],filter(lambda x:x[1]!=-1 or x[2]!=-1,zip(expPosList,self.expMatchList[0],self.expMatchList[1])))
+        #plt.plot(FNPosList,map(lambda x:rawSig[x],FNPosList),'kd',markersize = 16,label = 'False Negtive')
+        
+        # T predict list
+        prdPosList = self.leadPosList[0]
+        plt.plot(prdPosList,map(lambda x:rawSig[int(x)],prdPosList),'*',color = self.colors[3],label = 'prd1',markersize = 12)
+
+        prdPosList = self.leadPosList[1]
+        plt.plot(prdPosList,map(lambda x:rawSig[int(x)],prdPosList),'*',color = self.colors[4],label = 'prd2',markersize = 12)
+
+        # not matched list
+        #prdPosList = map(lambda x:x[0],filter(lambda x:x[1]==-1,zip(self.labellist,self.prdMatchList)))
+        #plt.plot(prdPosList,map(lambda x:rawSig[x],prdPosList),'k*',markersize = 14,label = 'False Positive')
+
+        # plot match line
+        #for expPos,matchInd in zip(self.expertlist,self.expMatchList):
+            #if matchInd == -1:
+                #continue
+            #prdPos = self.labellist[matchInd]
+            #plt.plot([expPos,prdPos],[rawSig[expPos],rawSig[prdPos]],lw = 14,color = self.colors[4],alpha = 0.3)
+
+        plt.grid(True)
+        plt.legend()
+        plt.title(self.recname)
+        plt.show()
+
+    def get_errList(self):
+        self.errList = []
+        FNcnt = 0
+        # plot match line
+        for expPos,matchInd1,matchInd2 in zip(self.expertlist,self.expMatchList[0],self.expMatchList[1]):
+            if matchInd1 == -1 and matchInd2 == -1:
+                FNcnt += 1
+                continue
+            prdpos1 = self.leadPosList[0][matchInd1]
+            prdpos2 = self.leadPosList[1][matchInd2]
+            err1 = expPos - prdpos1
+            err2 = expPos - prdpos2
+
+            # chooose the one with smaller error 
+            if abs(err1)<abs(err2):
+                self.errList.append(4.0*err1)
+            else:
+                self.errList.append(4.0*err2)
+
+        # total number of False Negtives
+        self.FNcnt = FNcnt
+
+        return self.errList
+
+    def get_total_mean(self):
+        meanVal = np.mean(self.errList)
+        return meanVal
+    def get_total_stdvar(self):
+        stdvar = np.std(self.errList)
+        return stdvar
+
+
+if __name__ == '__main__':
+    resultfilelist = glob.glob(os.path.join(curfolderpath,'MultiLead2','GroupResult','*.json'))
+    evalinfopath = os.path.join(curfolderpath,'MultiLead2','EvalInfo')
+
+    # print result files
+    for ind, fp in enumerate(resultfilelist):
+        print '[{}]'.format(ind),'fp:',fp
+
+    ErrDict = dict()
+    ErrData = dict()
+
+    for label in ['P','R','T','Ponset','Poffset','Toffset','Ronset','Roffset']:
+        ErrData[label] = dict()
+        ErrDict[label] = dict()
+        errList = []
+        FNcnt = 0
+
+        for file_ind in xrange(0,len(resultfilelist)):
+            # progress info
+            print 'label:',label
+            print 'file_ind',file_ind
+
+            eva= Evaluation2Leads()
+            eva.loadlabellist(resultfilelist[file_ind],label)
+            eva.evaluate(label)
+
+            # total error
+            errList.extend(eva.errList)
+            FN = eva.getFNlist()
+            FNcnt += FN
+
+            # -----------------
+            # error statistics
+
+            #ErrDict[label]['mean'] = np.mean(eva.errList)
+            #ErrDict[label]['std'] = np.std(eva.errList)
+            #ErrDict[label]['FN'] = eva.getFNlist()
+
+            # debug
+            #print '--'
+            #print 'record: {}'.format(os.path.split(resultfilelist[file_ind])[-1])
+            #print 'Error Dict:','label:',label
+            #print ErrDict[label]
+
+            # ======
+            #eva.plot_evaluation_result()
+            #pdb.set_trace()
+        ErrData[label]['errList'] = errList
+        ErrData[label]['FN'] = FNcnt
+
+        ErrDict[label]['mean'] = np.mean(errList)
+        ErrDict[label]['std'] = np.std(errList)
+        ErrDict[label]['FN'] = FNcnt
+
+    print '-'*10
+    print 'ErrDict'
+    print ErrDict
+
+    # write to json
+    with open(os.path.join(evalinfopath,'ErrData.json'),'w') as fout:
+        json.dump(ErrData,fout,indent = 4,sort_keys = True)
+        print '>>Dumped to json file: ''ErrData.json''.'
+    # error statistics
+    with open(os.path.join(evalinfopath,'ErrStat.json'),'w') as fout:
+        json.dump(ErrDict,fout,indent = 4,sort_keys = True)
+        print '>>Dumped to json file: ''ErrStat.json''.'
+
